b85a4e78f83658adab706e0122f2173f5b7fc96d
JS970 <son73097@pusan.ac.kr>
2023-04-24 21:47:57 +0900

    ^__^77

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -9,12 +9,12 @@ def do_fizzbuzz(num:int):
 	for i in range(1, num+1):
         if i%15==0:
             print('fizzbuzz')
-		elif i%3==0:
-			print('fizz')
+	elif i%3==0:
+		print('fizz')
         elif i%5==0:
             print('buzz')
-		else:
-			print(f'{i}')
+	else:
+		print(f'{i}')
 	print('hello')
 
 if __name__=='__main__':
